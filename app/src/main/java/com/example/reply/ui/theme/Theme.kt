c349cdc91e7b918d4dc9330976535f6f9bfc6dd6
iTovarr <13m.tovar.m@gmail.com>
2026-04-28 13:30:21 -0500

    ui: sync system status bar color with app primary theme

--- app/src/main/java/com/example/reply/ui/theme/Theme.kt
+++ app/src/main/java/com/example/reply/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.reply.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
 private val LightColors = lightColorScheme(
@@ -90,6 +95,17 @@ fun AppTheme(
         useDarkTheme -> DarkColors
         else -> LightColors
     }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.primary.toArgb()
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = !useDarkTheme
+        }
+    }
+
 
     MaterialTheme(
         colorScheme = colors,
